64f94fa48f7e10ce251805f3cb8d9736337b59f8
David Sowder <github.com@david.sowder.com>
2012-02-20 18:50:51 -0600

    pep8 compliance: W291 trailing whitespace

--- directories.py
+++ directories.py
@@ -17,48 +17,48 @@ import sys
 import os
 
 
-def win32_utf8_argv():                                                                                               
-    """Uses shell32.GetCommandLineArgvW to get sys.argv as a list of UTF-8                                           
-    strings.                                                                                                         
+def win32_utf8_argv():
+    """Uses shell32.GetCommandLineArgvW to get sys.argv as a list of UTF-8
+    strings.
 
-    Versions 2.5 and older of Python don't support Unicode in sys.argv on                                            
-    Windows, with the underlying Windows API instead replacing multi-byte                                            
-    characters with '?'.                                                                                             
+    Versions 2.5 and older of Python don't support Unicode in sys.argv on
+    Windows, with the underlying Windows API instead replacing multi-byte
+    characters with '?'.
 
-    Returns None on failure.                                                                                         
+    Returns None on failure.
 
-    Example usage:                                                                                                   
+    Example usage:
 
-    >>> def main(argv=None):                                                                                         
-    ...    if argv is None:                                                                                          
-    ...        argv = win32_utf8_argv() or sys.argv                                                                  
-    ...                                                                                                              
-    """                                                                                                              
+    >>> def main(argv=None):
+    ...    if argv is None:
+    ...        argv = win32_utf8_argv() or sys.argv
+    ...
+    """
 
-    try:                                                                                                             
-        from ctypes import POINTER, byref, cdll, c_int, windll                                                       
-        from ctypes.wintypes import LPCWSTR, LPWSTR                                                                  
+    try:
+        from ctypes import POINTER, byref, cdll, c_int, windll
+        from ctypes.wintypes import LPCWSTR, LPWSTR
 
-        GetCommandLineW = cdll.kernel32.GetCommandLineW                                                              
-        GetCommandLineW.argtypes = []                                                                                
-        GetCommandLineW.restype = LPCWSTR                                                                            
+        GetCommandLineW = cdll.kernel32.GetCommandLineW
+        GetCommandLineW.argtypes = []
+        GetCommandLineW.restype = LPCWSTR
 
-        CommandLineToArgvW = windll.shell32.CommandLineToArgvW                                                       
-        CommandLineToArgvW.argtypes = [LPCWSTR, POINTER(c_int)]                                                      
-        CommandLineToArgvW.restype = POINTER(LPWSTR)                                                                 
+        CommandLineToArgvW = windll.shell32.CommandLineToArgvW
+        CommandLineToArgvW.argtypes = [LPCWSTR, POINTER(c_int)]
+        CommandLineToArgvW.restype = POINTER(LPWSTR)
 
-        cmd = GetCommandLineW()                                                                                      
-        argc = c_int(0)                                                                                              
-        argv = CommandLineToArgvW(cmd, byref(argc))                                                                  
-        if argc.value > 0:                                                                                           
-#            # Remove Python executable if present                                                                    
-#            if argc.value - len(sys.argv) == 1:                                                                      
-#                start = 1                                                                                            
-#            else:                                                                                                    
-#                start = 0                                                                                            
-            return [argv[i] for i in                                                                 
-                    xrange(0, argc.value)]                                                                       
-    except Exception:                                                                                                
+        cmd = GetCommandLineW()
+        argc = c_int(0)
+        argv = CommandLineToArgvW(cmd, byref(argc))
+        if argc.value > 0:
+#            # Remove Python executable if present
+#            if argc.value - len(sys.argv) == 1:
+#                start = 1
+#            else:
+#                start = 0
+            return [argv[i] for i in
+                    xrange(0, argc.value)]
+    except Exception:
         pass
 
 
