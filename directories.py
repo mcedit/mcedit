9676496b1b5167c2db768ce49b3ed00fe9ce6277
David Sowder <github.com@david.sowder.com>
2012-02-20 18:47:19 -0600

    pep8 compliance: W293 blank line contains whitespace

--- directories.py
+++ directories.py
@@ -19,33 +19,33 @@ import os
 def win32_utf8_argv():                                                                                               
     """Uses shell32.GetCommandLineArgvW to get sys.argv as a list of UTF-8                                           
     strings.                                                                                                         
-                                                                                                                     
+
     Versions 2.5 and older of Python don't support Unicode in sys.argv on                                            
     Windows, with the underlying Windows API instead replacing multi-byte                                            
     characters with '?'.                                                                                             
-                                                                                                                     
+
     Returns None on failure.                                                                                         
-                                                                                                                     
+
     Example usage:                                                                                                   
-                                                                                                                     
+
     >>> def main(argv=None):                                                                                         
     ...    if argv is None:                                                                                          
     ...        argv = win32_utf8_argv() or sys.argv                                                                  
     ...                                                                                                              
     """                                                                                                              
-                                                                                                                     
+
     try:                                                                                                             
         from ctypes import POINTER, byref, cdll, c_int, windll                                                       
         from ctypes.wintypes import LPCWSTR, LPWSTR                                                                  
-                                                                                                                     
+
         GetCommandLineW = cdll.kernel32.GetCommandLineW                                                              
         GetCommandLineW.argtypes = []                                                                                
         GetCommandLineW.restype = LPCWSTR                                                                            
-                                                                                                                     
+
         CommandLineToArgvW = windll.shell32.CommandLineToArgvW                                                       
         CommandLineToArgvW.argtypes = [LPCWSTR, POINTER(c_int)]                                                      
         CommandLineToArgvW.restype = POINTER(LPWSTR)                                                                 
-                                                                                                                     
+
         cmd = GetCommandLineW()                                                                                      
         argc = c_int(0)                                                                                              
         argv = CommandLineToArgvW(cmd, byref(argc))                                                                  
@@ -59,7 +59,7 @@ def win32_utf8_argv():
                     xrange(0, argc.value)]                                                                       
     except Exception:                                                                                                
         pass
-        
+
 def findDirectories():
     #print 'CWD:', os.getcwdu()
 
