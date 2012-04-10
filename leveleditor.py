61754f3e16a34b6ed102268e16ae645faec14307
Jedediah Smith <jedediah@silencegreys.com>
2012-04-10 03:09:39 -0400

    fix mouse-look jumping after losing and regaining focus (probably only on Mac)

--- leveleditor.py
+++ leveleditor.py
@@ -2273,7 +2273,7 @@ class LevelEditor(GLViewport):
             self.dragInProgress = True
             self.dragStartPoint = (x,y)
             self.currentOperation.dragStart(x,y)
-      '
+      
     def mouseDragOff(self):
         if self.dragInProgress:
             self.dragInProgress = False
