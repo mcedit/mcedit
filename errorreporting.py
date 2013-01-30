2d6b4f8e2f10c46c669e844d192d508a0ec64735
David Vierra <codewarrior@hawaii.rr.com>
2013-01-30 13:55:41 -1000

    Debug: Change pyo and pyc to py when reporting errors.

--- errorreporting.py
+++ errorreporting.py
@@ -111,7 +111,7 @@ def get_backtrace():
     backtrace = []
     for filename, lineno, name, line, selfstr in reversed(extract_tb(tb)):
         backtrace.append({
-            "file":os.path.normpath(filename).replace(directories.dataDir, ""),
+            "file":os.path.normpath(filename).replace(directories.dataDir, "").replace(".pyo", ".py").replace(".pyc", ".py"),
             "line":lineno,
             "symbol":name,
         })
